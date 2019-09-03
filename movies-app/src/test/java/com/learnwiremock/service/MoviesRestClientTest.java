a7028bc975499165b2962cd0471f1920be82f1d1
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2019-09-03 05:51:03 -0500

    added the code to complete the verify

--- movies-app/src/test/java/com/learnwiremock/service/MoviesRestClientTest.java
+++ movies-app/src/test/java/com/learnwiremock/service/MoviesRestClientTest.java
@@ -418,7 +418,16 @@ class MoviesRestClientTest {
     void deleteMovieByName() {
 
         //given
-        //given
+        stubFor(post(urlPathEqualTo(ADD_MOVIE_V1))
+                .withRequestBody(matchingJsonPath("$.name", equalTo("Toy Story 5")))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.CREATED.value())
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBodyFile("toystory.json")));
+
+        stubFor(delete(urlPathMatching("/movieservice/v1/movieName/.*"))
+                .willReturn(WireMock.ok()));
+
         String toyStoryCrew = "Tom Hanks, Tim Allen";
         Movie toyStory = new Movie(null, "Toy Story 5", 2019, toyStoryCrew, LocalDate.of(2019, 06, 20));
         Movie movie = moviesRestClient.addNewMovie(toyStory);
@@ -426,7 +435,12 @@ class MoviesRestClientTest {
         //when
         String responseMessage = moviesRestClient.deleteMovieByName(movie.getName());
 
+        //then
         assertEquals("Movie Deleted SuccessFully", responseMessage);
+
+        verify(exactly(1), postRequestedFor(urlPathEqualTo(ADD_MOVIE_V1))
+                .withRequestBody(matchingJsonPath("$.name", equalTo("Toy Story 5"))));
+        verify(exactly(1), deleteRequestedFor((urlPathMatching("/movieservice/v1/movieName/.*"))));
 
     }
 
