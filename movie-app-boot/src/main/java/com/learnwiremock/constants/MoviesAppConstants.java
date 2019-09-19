ffdd453a707c842269d36b552462bd07237303c4
Dilip Sundarraj <dilip.sundarraj@target.com>
2019-09-19 09:20:17 -0500

    added the code to run wiremmock is junit4

--- movie-app-boot/src/main/java/com/learnwiremock/constants/MoviesAppConstants.java
+++ movie-app-boot/src/main/java/com/learnwiremock/constants/MoviesAppConstants.java
@@ -7,4 +7,6 @@ public class MoviesAppConstants {
     public static final String MOVIE_BY_NAME_QUERY_PARAM_V1 = "/movieservice/v1/movieName";
     public static final String MOVIE_BY_YEAR_QUERY_PARAM_V1 ="/movieservice/v1/movieYear";
     public static final String ADD_MOVIE_V1 ="/movieservice/v1/movie";
+    public static final String MOVIE_BY_NAME_PATH_PARAM_V1 = "/movieservice/v1/movieName/{name}";
+
 }
